29012d502b325095d6631b319e947de5ade00851
Marcel Törpe <info@frumania.com>
2021-12-21 09:52:29 +0100

    Update deploy.sh

--- cdk-iot-analytics/deploy.sh
+++ cdk-iot-analytics/deploy.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-mkdir certs
+set -e
 
-pip install -r requirements.txt
+if grep -R "<" cdk.json
+then
+   echo "cdk.json invalid! Kindly check and replace all parameters!"
+elif grep -R ">" cdk.json
+then
+   echo "cdk.json invalid! Kindly check and replace all parameters!"
+else
+    echo "cdk.json validation successful!"
 
-AWSACCOUNTID=$(aws sts get-caller-identity --query Account --output text)
+    mkdir -p certs
+    
+    pip install -r requirements.txt
+    
+    AWSACCOUNTID=$(aws sts get-caller-identity --query Account --output text)
+    
+    cdk bootstrap aws://$AWSACCOUNTID/us-east-1
+    
+    cdk deploy iot -O=iot-outputs.json --require-approval never
+    
+    pip install \
+        requests \
+        xmltodict \
+        -t ./cdk_sap_blog/sap/lambda_assets/layer/python/
+    
+    cdk deploy sap --require-approval never
+    
+    cdk deploy analytics -O=analytics-outputs.json --require-approval never
+    
+    sed -i 's/AWSACCOUNTID/'$AWSACCOUNTID'/g' cdk_sap_blog/analytics/detector_model.json
+    aws iotevents update-detector-model --cli-input-json file://cdk_sap_blog/analytics/detector_model.json
 
-cdk bootstrap aws://$AWSACCOUNTID/us-east-1
-
-cdk deploy iot -O=iot-outputs.json --require-approval never
-
-pip install \
-    requests \
-    xmltodict \
-    -t ./cdk_sap_blog/sap/lambda_assets/layer/python/
-
-cdk deploy sap --require-approval never
-
-cdk deploy analytics -O=analytics-outputs.json --require-approval never
-
-sed -i 's/AWSACCOUNTID/'$AWSACCOUNTID'/g' cdk_sap_blog/analytics/detector_model.json
-aws iotevents update-detector-model --cli-input-json file://cdk_sap_blog/analytics/detector_model.json
+fi
